0bdddab7c1fd62e954109a8c198e1e6255f577cf
dig1t <admin@javierm.net>
2022-08-12 23:55:33 -0500

    v1.0.0

--- src/Helper.lua
+++ src/Helper.lua
@@ -1,6 +1,6 @@
 -- src/UIKit/Modules/Helper.lua
 
-local Util = require(script.Parent.Parent.Util)
+local Util = require(script.Parent.dLib.Util)
 local Const = require(script.Parent.Constants)
 
 local Helper = {}
